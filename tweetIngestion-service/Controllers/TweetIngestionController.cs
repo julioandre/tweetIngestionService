d9ab8f83cfeed239ecd1dae6b7501460a253426a
julioandre275 <julioandre275@gmail.com>
2023-05-20 17:40:13 +0200

    Adding Cassandra Cluster

--- tweetIngestion-service/Controllers/TweetIngestionController.cs
+++ tweetIngestion-service/Controllers/TweetIngestionController.cs
@@ -1,4 +1,10 @@
+using System.Net.Security;
+using System.Security.Authentication;
+using Cassandra;
+using Cassandra.Mapping;
 using Microsoft.AspNetCore.Mvc;
+using ISession = Cassandra.ISession;
+using System.Security.Cryptography.X509Certificates;
 
 namespace tweetIngestion_service.Controllers;
 
@@ -6,5 +12,33 @@ namespace tweetIngestion_service.Controllers;
 [Route("/api/[controller]")]
 public class TweetIngestionController:ControllerBase
 {
-    
+    private ICluster _cluster;
+    private ISession _session;
+    private IMapper _mapper;
+
+    public TweetIngestionController(ICluster cluster, ISession session)
+    {
+        var options = new Cassandra.SSLOptions(SslProtocols.Tls12, true, ValidateServerCertificate);
+        options.SetHostNameResolver((ipAddress) => "julioandre1.cassandra.cosmos.azure.com");
+        _cluster = Cluster.Builder()
+            .WithCredentials("julioandre1", "k0CHWInzxVJ3QlT6d2zPFW6WNkkuPs0sewU633IGfPdfoV0mpMlZih3rKXNym1iUiQjqHznXOYewACDbXoKsKQ==")
+            .WithPort(10350)
+            .AddContactPoint("julioandre1.cassandra.cosmos.azure.com")
+            .WithSSL(options)
+            .Build();
+        _session = _cluster.Connect("tweeter");
+    }
+    public static bool ValidateServerCertificate(
+        object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors)
+    {
+        if (sslPolicyErrors == SslPolicyErrors.None)
+            return true;
+
+        Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+        // Do not allow this client to communicate with unauthenticated servers.
+        return false;
+    }
 }
--- tweetIngestion-service/Program.cs
+++ tweetIngestion-service/Program.cs
@@ -1,3 +1,4 @@
+using Cassandra;
 using tweetIngestion_service.Data;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("tweetingestion-db-connection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseCassandra(connectionString:"HostName=julioandre1.cassandra.cosmos.azure.com;Username=julioandre1;Password=k0CHWInzxVJ3QlT6d2zPFW6WNkkuPs0sewU633IGfPdfoV0mpMlZih3rKXNym1iUiQjqHznXOYewACDbXoKsKQ==;Port=10350",defaultKeyspace:"tweeter",cassandraOptionsAction:null));
 
 var app = builder.Build();
 
